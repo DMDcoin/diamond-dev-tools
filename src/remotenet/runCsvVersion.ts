a292236f8ea1dc0a0a29532cca2381d75c0ced6e
Thomas Haller <thomashaller@gmx.at>
2025-02-12 14:20:36 +0100

    remotenet-csv-version: added version and date

--- src/remotenet/runCsvVersion.ts
+++ src/remotenet/runCsvVersion.ts
@@ -6,6 +6,30 @@ import { cmdR } from "../remoteCommand";
 import { getNodesFromCliArgs } from "./remotenetArgs";
 import { getNodeVersion } from "./getNodeVersion";
 
+
+function parseVersion(version: string) {
+
+  //Example: OpenEthereum/v3.3.5-hbbft-0.9.7-unstable-d8b55f726-20250210/x86_64-linux-gnu/rustc1.75.0
+
+  try {
+
+    const parts = version.split("/");
+    const versionPart = parts[1];
+    const versionParts = versionPart.split("-");
+
+    const date = versionParts[versionParts.length - 1];
+    
+    const commit = versionParts[versionParts.length - 2];
+
+    return { date, commit };
+
+  } catch {
+    
+    return { date: "", commit: "" };
+  }
+  
+}
+
 async function run() {
 
   const nodes = await getNodesFromCliArgs();
@@ -27,6 +51,8 @@ async function run() {
 
 
     let version = getNodeVersion(nodeName);
+
+    let parsedVersion = parseVersion(version);
 
     let isAvailable = false;
     let isStaked = false;
@@ -59,10 +85,10 @@ async function run() {
 
     const sha1binaryDuo = cmdR(nodeName, `sha1sum ~/${ConfigManager.getNetworkConfig().installDir}/diamond-node`);
     const sha1binary = sha1binaryDuo.split(" ")[0];
-    csvLines.push(`"${n.sshNodeName()}";"${current}";"${isAvailable}";"${isStaked}";"${stakeString}";"${n.address}";"${poolAddress}";"${sha1binary}";"${version}";`);
+    csvLines.push(`"${n.sshNodeName()}";"${current}";"${isAvailable}";"${isStaked}";"${stakeString}";"${n.address}";"${poolAddress}";"${sha1binary}";"${version}";"${parsedVersion.date}";"${parsedVersion.commit}";`);
   }
 
-  console.log('"node";"current";"available";"staked";"stake";"address";"poolAddress", "sha1binary"; "version";');
+  console.log('"node";"current";"available";"staked";"stake";"address";"poolAddress", "sha1binary"; "version";"versionDate";"versionCommit";');
   csvLines.forEach(x => console.log(x));
 }
 
