09780bcdfe4a3f9e53e82894c6cb7f380f1a82d5
Thomas Haller <thomashaller@gmx.at>
2025-02-22 22:47:39 +0100

    analyse nonce gaps

---
+++ src/net/runAnalyzeNonceGaps.ts
@@ -0,0 +1,88 @@
+import { Transaction } from "web3-core";
+import { ConfigManager } from "../configManager";
+import { getPendingTransactions } from "../tools/web3tools";
+import { ContractManager } from "../contractManager";
+
+
+// function groupBy(list, keyGetter) {
+//     const map = new Map();
+//     list.forEach((item) => {
+//          const key = keyGetter(item);
+//          const collection = map.get(key);
+//          if (!collection) {
+//              map.set(key, [item]);
+//          } else {
+//              collection.push(item);
+//          }
+//     });
+//     return map;
+// }
+
+
+async function run() {
+
+    console.log("started");
+
+    const web3 = ConfigManager.getWeb3();
+
+    //web3.eth.getPendingTransactions
+
+    
+    // web3.currentProvider.
+
+     let pending: Array<Transaction> = await getPendingTransactions(web3);
+
+     console.log("got pending transaction: ", pending.length);
+    // try {
+    //     pending = await web3.eth.getPendingTransactions();
+    // } catch(e) { 
+    //     console.log("BEGIN     RPC Error fetching. ", e);
+    //     console.log("END       RPC Error fetching. ");
+    //     return;
+    // }
+    // get all unique addresses from pending transactions.
+
+//    console.log("pending:", pending.length);
+
+    const addresses = new Map<string, Transaction[] >();
+
+    pending.forEach(x => {
+        
+        let existing = addresses.get(x.from);
+        if (!existing) {
+            existing = [x];
+            addresses.set(x.from, existing);
+        }
+        else {
+            existing.push(x);
+        };
+    });
+
+    const contractManager = ContractManager.get();
+    const permissionContract = contractManager.getContractPermission();
+    
+    for (const x of addresses) {
+        // x[0]
+
+        // conver x[1] to array
+        // sort by nonce
+        // find gaps
+        // print gaps
+
+        const transactions = x[1].sort((a, b) => a.nonce - b.nonce);
+        const address = x[0];
+        const transactionCount = await web3.eth.getTransactionCount(address);
+
+        console.log("address", address );
+        console.log("transactionCount", transactionCount );
+        transactions.forEach(async x => {
+
+            console.log(x.hash + " " + x.input );
+            const allowed = await permissionContract.methods.allowedTxTypes(x.from, x.to!, x.value, x.gasPrice, x.input).call();
+            console.log(x.nonce + " - " + x.hash + " - ", allowed.typesMask);
+        });
+    }
+}
+
+
+run();
