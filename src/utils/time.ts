f187a87fb6327d8966eb12cdce1a28c2d1d2011e
Thomas Haller <thomashaller@gmx.at>
2025-07-30 23:26:30 +0200

    base functions for new tests

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -3,3 +3,12 @@
 export function sleep(milliseconds: number) {
   return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
+
+/// checks the condition every `milliseconds` milliseconds until it returns true.
+export async function spoolWait(milliseconds: number, condition: () => Promise<boolean>) {
+
+  while (!await condition()) {
+    await sleep(milliseconds);
+  }
+  
+}
