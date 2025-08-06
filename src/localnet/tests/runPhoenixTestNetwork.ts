1607862137510fe141b1fca10244e6355287928b
Thomas Haller <thomashaller@gmx.at>
2025-08-06 21:45:55 +0200

    phoenix implementation

--- src/localnet/tests/runPhoenixTestNetwork.ts
+++ src/localnet/tests/runPhoenixTestNetwork.ts
@@ -5,6 +5,7 @@ import {
 } from "./localnetBootstrapper";
 import { sleep, spoolWait } from "../../utils/time";
 import { ContractManager } from "../../contractManager";
+import { NodeRunStateExecuter, NodeRunStateRandomWalkGenerator, NodeRunStateTestSpecification } from "./nodeRunStateTestSpecification";
 
 export class PhoenixTestRunner extends LocalnetScriptRunnerBase {
 
@@ -37,6 +38,15 @@ export class PhoenixTestRunner extends LocalnetScriptRunnerBase {
 
   async runImplementation(): Promise<boolean> {
 
+
+    let randomStates = new  NodeRunStateRandomWalkGenerator().generate(1000, 7);
+
+    console.log(
+      `Random walk generated with ${randomStates.length} entries, starting to execute it.`
+    );
+    NodeRunStateExecuter.exportEntriesToCsvFile(randomStates, "phoenix-test-random-walk.csv");
+
+
     const contractManager = new ContractManager(this.web3);
 
     const epochOnStartup = await contractManager.getEpoch("latest");
@@ -44,41 +54,76 @@ export class PhoenixTestRunner extends LocalnetScriptRunnerBase {
     console.log("epoch on startup:", epochOnStartup.toString());
 
     
-    let nodesToStop = [2, 3, 4];
-    console.log(
-      `stopping Node ${nodesToStop} block creation should fail.`
-    );
+    //let nodesToStop = [2, 3, 4];
+    // console.log(
+    //   `stopping Node ${nodesToStop} block creation should fail.`
+    // );
 
-    await this.stopNodes(nodesToStop);
+   // await this.stopNodes(nodesToStop);
 
-    console.log(
-      "creating block, that cannot be mined (yet)."
-    );
+    // console.log(
+    //   "creating block, that cannot be mined (yet)."
+    // );
     
-    let block = this.createBlock();
+   
 
     const waitTimeForBlockCreation = 1000;
     
     await sleep(waitTimeForBlockCreation);
 
 
-    this.startNodes(nodesToStop);
-      
-    await this.restartSet([5,6,7]);
-    await this.restartSet([2,3,4]);
-    // await this.restartSet([5,6,7]);
 
+    let specification : NodeRunStateTestSpecification  = {
+      entries: randomStates
+    };
+    let runStateExecutor = new NodeRunStateExecuter(specification, this.currentNodeManager);
+
+   
+
+   //await this.stopNodes([5, 6 , 7]);
+    
+
+   // this.startNodes(nodesToStop);
+    
+
+   // await this.startNodes([5, 6 , 7]);
+
+    // await this.restartSet([5,6,7]);
+    // await this.restartSet([5,6,7]);
+    // await this.restartSet([2,3,4]);
+    // // await this.restartSet([5,6,7]);
+
+
+
+    // let timeout = setTimeout(() => {
+    //   this.handleTimoutError();
+    // }, 30000);
+
+
+    console.log("triggering block production");
+    let unminedBlock: Promise<void> = this.createBlock(); 
+    //await unminedBlock;//  spoolWait(1000, async () => await contractManager.getEpoch("latest") == epochOnStartup + 1);
+
+    //clearTimeout(timeout);
+
+    while (!runStateExecutor.finished()) {
+      
+      let shouldProduceBlock = await runStateExecutor.next();
+
+      console.log(
+        "....."
+      );
+
+      if (shouldProduceBlock) {
+        console.log("state ", runStateExecutor.currentStep, " should produce a block awaiting....");
+        await unminedBlock;
+        unminedBlock = this.createBlock();
+      }
+    }
+    
     console.log(
       "phoenix protocol should now detect a stalled network and therefore should trigger the ."
     );
-    
-    let timeout = setTimeout(() => {
-      this.handleTimoutError();
-    }, 30000);
-
-    await block;//  spoolWait(1000, async () => await contractManager.getEpoch("latest") == epochOnStartup + 1);
-
-    clearTimeout(timeout);
     
     return true;
   }
