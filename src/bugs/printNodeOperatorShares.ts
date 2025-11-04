7134100f8d66b5ed7392563dcb650069aba7dc97
Thomas Haller <thomashaller@gmx.at>
2025-11-04 15:26:42 +0100

    incident 22 script

--- src/bugs/printNodeOperatorShares.ts
+++ src/bugs/printNodeOperatorShares.ts
@@ -8,17 +8,40 @@ async function printNodeOperatorShares() {
     const web3 = ConfigManager.getWeb3();
 
     const cm = ContractManager.get();
-
+    const staking = await cm.getStakingHbbft();
     const allPools = await cm.getAllPools();
 
+    console.log("stakes:");
     for (const pool of allPools) {
-
-        //const operator = cm.getPoolOperatorAddress(pool);
-        //const shares = await pool.getOperatorShares();
-        //console.log(`Pool: ${pool.address} | Operator: ${operator} | Shares: ${web3.utils.fromWei(shares.toString(), "ether")} DMD`);
 
         const shareAddress = await cm.getPoolOperatorAddress(pool);
         console.log(pool, " -> ", shareAddress);
+
+        const totalStake = await cm.getTotalStake(pool);
+        console.log(pool, " -> ", totalStake.toString(10));
+        let totalStakeCalced = web3.utils.toBN(0);
+        console.log(pool, " (", shareAddress, ")", " => ", web3.utils.fromWei(totalStake.toString(10), "ether"));
+        const poolDelegators = await staking.methods.poolDelegators(pool).call();
+        for (const dele of poolDelegators) {
+            const stake = await staking.methods.stakeAmount(pool, dele).call();
+            const orderedWithdrawAmount = await cm.getOrderedWithdrawalAmount(pool, pool);
+            totalStakeCalced = totalStakeCalced.add(web3.utils.toBN(stake));
+            console.log("   delegator ", dele, " stake: ", web3.utils.fromWei(stake, "ether"), " withdraw ordered: ", web3.utils.toWei(orderedWithdrawAmount.toString(), "ether"));
+        }
+
+
+        let stakeAmountSelf = await staking.methods.stakeAmount(pool, pool).call(); 
+        totalStakeCalced = totalStakeCalced.add(web3.utils.toBN(stakeAmountSelf));
+        console.log("self: ", stakeAmountSelf);
+
+
+        console.log("   pool owner withdraw order: ",);
+        //contractManager.getStakingHbbft(;)
+        console.log("   calced total stake: ", web3.utils.fromWei(totalStakeCalced.toString(), "ether"));
+        console.log(" total stake:",  web3.utils.fromWei(totalStake.toString(10), "ether"));
+        const diff = totalStakeCalced.sub(web3.utils.toBN(totalStake.toString(10)));
+        console.log("   difference", web3.utils.fromWei(diff.toString(10), "ether"));
+        console.log("======");
     }
 }
 
